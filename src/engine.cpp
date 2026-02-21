a6e44e821ae6bd3b3dfc1040a6eaea1d657976b4
majockbim <majock.bim1@gmail.com>
2026-02-21 03:57:53 -0500

    feat/Implement a singleton design patter for the audio engine

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,53 +1,62 @@
-#include <inc/engine.hpp>
+#include "../inc/engine.hpp"
 
+class AudioEngine {
+public:
+    AudioEngine(const AudioEngine&) = delete; // remove copy constructor
+    AudioEngine(AudioEngine&&) = delete; // remove move constructor
 
-/*
-#include <iostream>
-#include <inc/visualizer.hpp>
-#include <inc/engine.hpp>
+    // Access class statically
+    static AudioEngine& Get()
+    {
+        static AudioEngine instance;
+        return instance;
+    }
 
-int main() {
-    CoInitialize(NULL);
+    float GenVolLevel() { return Get().InternalGenVol(); }
+
+private:
+    // Initialize object
+    AudioEngine() 
+    { 
+        CoInitialize(NULL);
+
+        // Get the default audio endpoint
+        CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+        pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+
+        // Activate volume interface
+        pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, NULL, (void**)&pEndpointVolume);
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/endpointvolume/nf-endpointvolume-iaudioendpointvolume-getchannelcount
+        pEndpointVolume->GetChannelCount(&ChannelCount);
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/endpointvolume/nf-endpointvolume-iaudioendpointvolume-getchannelcount
+        HRESULT result = pEndpointVolume->GetChannelCount(&ChannelCount);
+    }
     
-    // Initializations
+    // Initialize member variables
     IMMDeviceEnumerator* pEnumerator = NULL;
     IMMDevice* pDevice = NULL;
     IAudioEndpointVolume* pEndpointVolume = NULL;
 
-    // Get the default audio endpoint
-    CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
-    pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
-
-    // Activate volume interface
-    pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, NULL, (void**)&pEndpointVolume);
-
-    // Get a count of channels in audio stream that enter/leave audio enpoint
+    // Count of channels in audio stream that enter/leave audio enpoint
     UINT ChannelCount = 0;
 
-    // https://learn.microsoft.com/en-us/windows/win32/api/endpointvolume/nf-endpointvolume-iaudioendpointvolume-getchannelcount
-    HRESULT result = pEndpointVolume->GetChannelCount(&ChannelCount);
-
-    std::cout << "Result: " << result << "\n";
-    std::cout << "Channel Count: " << ChannelCount;
-
-
-    // Print volume level
-    while(true) {
-
-        float vol = 0;
-
-        pEndpointVolume->GetChannelVolumeLevelScalar(0, &vol);
-
-        std::cout << vol << "\n";
-
+    // Cleanup
+    ~AudioEngine()
+    {
+        pEndpointVolume->Release();
+        pDevice->Release();
+        pEnumerator->Release();
+        CoUninitialize();
     }
 
-    // cleanup
-    pEndpointVolume->Release();
-    pDevice->Release();
-    pEnumerator->Release();
-    CoUninitialize();
+    float InternalGenVol() 
+    { 
+        float vol = 0;
 
-    return 0;
-}
-*/
+        // Volume for channel 0
+        return pEndpointVolume->GetChannelVolumeLevelScalar(0, &vol);
+    }
+
+};
