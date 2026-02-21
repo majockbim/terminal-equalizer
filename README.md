66429a83b0b082a3c9cf013d040cb741fd0a8828
majockbim <majock.bim1@gmail.com>
2026-02-21 02:06:33 -0500

    refactor/modify project files + implement Separation of Concerns

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 pet project written in C++17
 
+# high level analysis of the application:
+main.cpp: entry point of the application, calls the necessary functions to initialize the application and start the main loop.
+visualizer.cpp: contains the code for the visualizer, which is responsible for displaying the audio levels in a graphical way.
+engine.cpp: contains the code for the audio engine, which is responsible for capturing the audio levels and providing them to the visualizer.
+
 readings:
 https://learn.microsoft.com/en-us/windows/win32/api/endpointvolume/nn-endpointvolume-iaudioendpointvolume
--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,15 @@
+#include "../inc/visualizer.hpp"
 #include <iostream>
-#include <windows.h>
-#include <mmdeviceapi.h>
-#include <endpointvolume.h>
 
 int main() {
-    CoInitialize(NULL);
-    
-    // Initializations
-    IMMDeviceEnumerator* pEnumerator = NULL;
-    IMMDevice* pDevice = NULL;
-    IAudioEndpointVolume* pEndpointVolume = NULL;
+   
+    // Init visualizer object
+    RenderEqualizer test;
 
-    // Get the default audio endpoint
-    CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
-    pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
-
-    // Activate volume interface
-    pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, NULL, (void**)&pEndpointVolume);
-
-    // Get a count of channels in audio stream that enter/leave audio enpoint
-    UINT ChannelCount = 0;
-
-    // https://learn.microsoft.com/en-us/windows/win32/api/endpointvolume/nf-endpointvolume-iaudioendpointvolume-getchannelcount
-    HRESULT result = pEndpointVolume->GetChannelCount(&ChannelCount);
-
-    std::cout << "Result: " << result << "\n";
-    std::cout << "Channel Count: " << ChannelCount;
-
-
-    // Print volume level
-    while(true) {
-
-        float vol = 0;
-
-        pEndpointVolume->GetChannelVolumeLevelScalar(0, &vol);
-
-        std::cout << vol << "\n";
-
+    // continuous loop displaying data
+    for(;;) {
+        std::cout << test.Get() << "\n";
     }
-
-    // cleanup
-    pEndpointVolume->Release();
-    pDevice->Release();
-    pEnumerator->Release();
-    CoUninitialize();
 
     return 0;
 }
