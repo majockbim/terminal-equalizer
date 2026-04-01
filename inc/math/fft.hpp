34f942f3fb7dcca99ccdc4e2c9924bacc13e125a
majockbim <majock.bim1@gmail.com>
2026-04-01 08:31:57 -0400

    refactor/Initialize FFTEngine class, for 1D real to complex transformation

--- inc/math/fft.hpp
+++ inc/math/fft.hpp
@@ -22,4 +22,13 @@ using namespace fftwpp;
 using namespace Array; 
 using namespace parallel;
 
-void FFT() {}
+class FFTEngine {
+private:
+    size_t n = 2400; // input sample (real)
+    size_t np = n / 2 + 1; // returning sample size (real + complex)
+    size_t align = sizeof(Complex);
+public:
+    FFTEngine();
+    ~FFTEngine();
+    std::array<double, 2400> Run(std::array<double, 2400>& audioBuffer);
+};
--- src/fft.cpp
+++ src/fft.cpp
@@ -1,20 +1,16 @@
 #include "../inc/math/fft.hpp"
 
-void FFT()
+FFTEngine::FFTEngine()
 {
     size_t n = 2400; // input sample (real)
     size_t np = n / 2 + 1; // returning sample size (real + complex)
     size_t align = sizeof(Complex);
 
-    array1<Complex> F(np, align); // returned
-    array1<double> f(n, align); // input array of 2400 samples
+    array1<double> f(n, align);
+    array1<Complex> F(np, align);
+}
 
-    rcfft1d Forward(n, f, F);
-
-    for(size_t i = 0; i < n; ++i) f[i] = i;
-
-    std::cout << "Input: " << f << "\n";
-    std::cout << "Output: " << F << "\n";
-
+std::array<double, 2400> FFTEngine::Run(std::array<double, 2400>& audioBuffer)
+{
 
 }
