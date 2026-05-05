d81446fb3fc38030407d7633f0c9a5efd368044e
Joe Independent Developer <johnmilson125@gmail.com>
2026-05-05 11:13:16 +0100

    Update visualizer.hpp

--- inc/ui/visualizer.hpp
+++ inc/ui/visualizer.hpp
@@ -27,23 +27,21 @@
 #include <cmath>
 #include <chrono>
 #include <algorithm>
-
-#include <iostream>
-#include <time.h>
-#include <stdlib.h>
-#include <cmath>
+#include "../inc/settings/settings.h"
 
 class RenderEqualizer {
 public:
     void Display(); // general volume level
     void DisplayBuffer(); // print float values of buffer
 
-    void EnableVisualizer(std::vector<double>& freq, std::mutex& magMutex, int sampleRate);
+    void EnableVisualizer(std::vector<double>& freq, std::mutex& magMutex, int sampleRate, const Dev::JoerAndMj::SettingsJsonFileReader& jsonFileReader);
 
 private:
     CONSOLE_SCREEN_BUFFER_INFO csbi;        
     int termWidth;
     int termHeight;
+    int lastHeight;
+    int lastWidth;
 
     int N_BARS;
 
